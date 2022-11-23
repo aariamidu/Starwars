b5967c05e928304caf250619eb9a5e6da02e80d4
aariamidu <aarifah.amidu@gmail.com>
2022-11-23 16:48:33 +0000

    added scoring system
    broke code while trying to add scoring system so rewritten from already save and added scoring system

--- starwars.py
+++ starwars.py
@@ -11,54 +11,45 @@ def random_person():
         'name': person['name'],
         'height': person['height'],
         'mass': person['mass'],
-        'birth_year': person['birth_year'],
+        'birth year': person['birth_year'],
     }
 
 
 def run():
+
     print('Hello stranger, Welcome to StarWars Top Trump!')
     player_name = input('What is your name?')
     lives_remaining = 3
+    score = 0
     while lives_remaining > 0:
         my_person = random_person()
-    print(player_name, ',you were given {}'.format(my_person['name']))
-    stat_choice = input('Which stat do you want to use? ( height, mass, birth_year)')
-    opponent_person = random_person()
-    print('The opponent chose {}'.format(opponent_person['name']))
-    my_stat = my_person[stat_choice]
-    opponent_stat = opponent_person[stat_choice]
-    if my_stat > opponent_stat:
-        print(player_name, ',You Win! 🙌🏽')
-    elif my_stat < opponent_stat:
-        print(player_name, ',You Lose! 🥺Try Again')
-    else:
-        print('Its A Draw!')
+        print(player_name, ', you were given', my_person['name'])
 
-    my_person = random_person()
-    print(player_name, ',you were given {}'.format(my_person['name']))
-    stat_choice = input('Which stat do you want to use? ( height, mass, birth_year)')
-    opponent_person = random_person()
-    print('The opponent chose {}'.format(opponent_person['name']))
-    if my_stat > opponent_stat:
-        print(player_name, ',You Win! 🙌🏽')
-    elif my_stat == opponent_stat:
-        print('Its A Draw!')
-    elif my_stat < opponent_stat:
-        lives_remaining -= 1
-        print(player_name, 'You have', lives_remaining, 'lives remaining!')
+        while True:
+            stat_choice = input('Which stat do you want to use? ( height, mass, birth year)')
+            if stat_choice.lower() not in ('height', 'mass', 'birth year'):
+                print('Not an appropriate answer. Try again.')
+            else:
+                break
 
+        opponent_person = random_person()
+        print('The opponent chose', opponent_person['name'])
+        my_stat = my_person[stat_choice]
+        opponent_stat = opponent_person[stat_choice]
 
-    my_person = random_person()
-    print(player_name, ',you were given {}'.format(my_person['name']))
-    stat_choice = input('Which stat do you want to use? ( height, mass, birth_year)')
-    opponent_person = random_person()
-    print('The opponent chose {}'.format(opponent_person['name']))
-    if my_stat > opponent_stat:
-        print(player_name, ',You Win! 🙌🏽')
-    elif my_stat < opponent_stat:
-        print(player_name, ',You Lose! 🥺Try Again')
-    else:
-        print('Its A Draw!')
+        if my_stat > opponent_stat:
+            print(player_name, 'You Win! 🙌🏽')
+            score = score + 1
+            print(player_name, 'You have ', lives_remaining, 'lives remaining!')
+            print('Your score is', score)
+        elif my_stat == opponent_stat:
+            print('Its A Draw!')
+            print(player_name, 'You have', lives_remaining, 'lives remaining!')
+            print('Your score is', score)
+        elif my_stat < opponent_stat:
+            lives_remaining = lives_remaining - 1
+            print(player_name,  'You have', lives_remaining, 'lives remaining!')
+            print('Your score is', score)
 
 
 run()
